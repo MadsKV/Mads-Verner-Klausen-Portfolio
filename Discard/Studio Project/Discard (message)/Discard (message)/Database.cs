46eea12a87b5d13e9d649b3f40ad9f34ec9fbd45
Mads <mads.v.k@hotmail.com>
2020-09-08 14:42:00 +0200

    hejsa

---
+++ Discard/Studio Project/Discard (message)/Discard (message)/Database.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TECHCOOL;
+
+namespace Discard__message_
+{
+    class Database
+    {
+        public static void SetupSQLServer()
+        {
+            try
+            {
+
+            SQLet.Execute(@"CREATE TABLE Users (
+                Author_ID int primary key IDENTITY(1,1) NOT NULL,
+                Author_Name nvarchar(32) NOT NULL
+                )");
+
+            SQLet.Execute(@"CREATE TABLE Messages (
+                Message_ID int primary key IDENTITY(1,1) NOT NULL,
+                Message_Date datetime NOT NULL DEFAULT GETDATE(),
+                Message_Text text NOT NULL,
+                Message_Author_ID int NOT NULL CONSTRAINT FK_user_message FOREIGN KEY (Message_Author_ID)
+                    REFERENCES Users
+                    ON DELETE Cascade
+                    ON Update Cascade
+                )");
+            }
+            catch(Microsoft.Data.SqlClient.SqlException)
+            {
+
+            }
+        }
+    }
+}
---
+++ Discard/Studio Project/Discard (message)/Discard (message)/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using TECHCOOL;
+
+namespace Discard__message_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SQLet.ConnectSqlServer("Discard", @"MADS-V-KLAUSEN\MSSQLSERVER01");
+
+            const ConsoleKey keyInfo1 = ConsoleKey.D1;
+            const ConsoleKey keyInfo2 = ConsoleKey.D2;
+            const ConsoleKey keyInfo3 = ConsoleKey.D3;
+            const ConsoleKey keyInfo4 = ConsoleKey.D4;
+            const ConsoleKey keyInfo5 = ConsoleKey.D5;
+            const ConsoleKey keyInfo6 = ConsoleKey.D6;
+            const ConsoleKey keyInfo7 = ConsoleKey.D7;
+            const ConsoleKey keyInfo8 = ConsoleKey.D8;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.ForegroundColor
+                = ConsoleColor.DarkGray;
+                MessagesMethods.SelectAllMessage();
+                Console.WriteLine("Press 1 :Insert Message.\nPress 2 :Delete Message.\nPress 3 :Update Message.\nPress 4 :Show Message from user.\n\nPress 5 :Insert new Author.\nPress 6 :Delete Author.\nPress 7 :Update Author.\nPress 8 :Show Message from Author.\n");
+
+                ConsoleKey pressedKey = PressedKey();
+
+                switch (pressedKey)
+                {
+                    case keyInfo1:
+                        Console.ForegroundColor
+                        = ConsoleColor.Blue;
+                        Console.Write("Enter new message > ");
+                        string input = Console.ReadLine();
+                        MessagesMethods.insertMessage(input, 1);
+                        break;
+
+                    case keyInfo2:
+                        Console.ForegroundColor
+                        = ConsoleColor.Blue;
+                        Console.Write("Specify what Message_ID you want to delete > ");
+                        string inputdelete = Console.ReadLine();
+                        MessagesMethods.deleteMessage(inputdelete);
+                        break;
+
+                    case keyInfo3:
+                        Console.ForegroundColor
+                        = ConsoleColor.Blue;
+                        Console.Write("What Message_ID do you want to update > ");
+                        string inputUpdate = Console.ReadLine();
+                        Console.Write("What do you want to update the message to > ");
+                        string inputMessage = Console.ReadLine();
+                        MessagesMethods.updateMessage(inputUpdate, inputMessage);
+                        break;
+
+                    case keyInfo4:
+                        Console.ForegroundColor
+                        = ConsoleColor.Green;
+                        Console.Write("What Author_ID do you want to see messages from? > ");
+                        string aInputID = Console.ReadLine();
+                        MessagesMethods.SelectMessage(aInputID);
+                        break;
+
+                    case keyInfo5:
+                        Console.ForegroundColor
+                        = ConsoleColor.Green;
+                        Console.Write("Enter new Author Name > ");
+                        string Authorinput = Console.ReadLine();
+                        UserMethods.insertAuthor(Authorinput);
+                        break;
+
+                    case keyInfo6:
+                        Console.ForegroundColor
+                        = ConsoleColor.Green;
+                        Console.Write("Specify what Author_ID you want to delete > ");
+                        string Authorinputdelete = Console.ReadLine();
+                        UserMethods.deleteAuthor(Authorinputdelete);
+                        break;
+
+                    case keyInfo7:
+                        Console.ForegroundColor
+                        = ConsoleColor.Green;
+                        Console.Write("What Author_ID do you want to update > ");
+                        string AuthorinputUpdate = Console.ReadLine();
+                        Console.Write("What do you want to update the Author_Name to > ");
+                        string AuthorinputMessage = Console.ReadLine();
+                        UserMethods.updateAuthor(AuthorinputUpdate, AuthorinputMessage);
+                        break;
+
+                    case keyInfo8:
+                        Console.ForegroundColor
+                        = ConsoleColor.Green;
+                        Console.Write("What Author_ID do you want to see messages from(Under construction)? > ");
+                        string AuthoraInputID = Console.ReadLine();
+                        UserMethods.SelectAuthor(AuthoraInputID);
+                        break;
+                }
+            }
+            ConsoleKey PressedKey()
+            {
+                do
+                {
+                    while (!Console.KeyAvailable) ;
+                    ConsoleKey pressed = Console.ReadKey(true).Key;
+                    if (pressed == keyInfo1 || pressed == keyInfo2 || pressed == keyInfo3 || pressed == keyInfo4 || pressed == keyInfo5 || pressed == keyInfo6 || pressed == keyInfo7 || pressed == keyInfo8)
+                        return pressed;
+                    Console.Clear();
+                } while (true);
+            }
+        }
+    }
+}
+
