3c95f1a93e858350025e3b4b71d0a192753a2f87
Mads <mads.v.k@hotmail.com>
2020-10-01 10:54:51 +0200

    RazorPage

---
+++ Razor/RazorStart/RazorStart/Data/RazorStartContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorStart.Data
+{
+    public class RazorStartContext : DbContext
+    {
+        public RazorStartContext (DbContextOptions<RazorStartContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Razor/RazorStart/RazorStart/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+using RazorStart.Data;
+
+namespace RazorStart.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RazorStart.Data.RazorStartContext _context;
+
+        public DetailsModel(RazorStart.Data.RazorStartContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
