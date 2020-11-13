772c3fc3ed1d725f41ffef3bbc05924a8441b68d
Mads <mads.v.k@hotmail.com>
2020-11-13 10:47:10 +0100

    blah

--- JavaScript Projects/Battle-Arena/js/gamemanager.js
+++ JavaScript Projects/Battle-Arena/js/gamemanager.js
@@ -76,20 +76,86 @@ let GameManager = {
       "</p></div>";
   },
   setPreFight: function () {
-      let getHeader = document.querySelector(".header");
-      let getActions = document.querySelector(".actions");
-      let getArena = document.querySelector(".arena");
+    let getHeader = document.querySelector(".header");
+    let getActions = document.querySelector(".actions");
+    let getArena = document.querySelector(".arena");
 
-      getHeader.innerHTML = '<p>Task: Find an enemy!</p>';
-      getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="GameManager.setFight()">Search for enemy</a>';
-      getArena.style.visibillity = "visible";
+    getHeader.innerHTML = "<p>Task: Find an enemy!</p>";
+    getActions.innerHTML =
+      '<a href="#" class="btn-prefight" onclick="GameManager.setFight()">Search for enemy</a>';
+    getArena.style.visibillity = "visible";
   },
-  setFight: function() {
+  setFight: function () {
     let getHeader = document.querySelector(".header");
     let getActions = document.querySelector(".actions");
     let getEnemy = document.querySelector(".enemy");
 
-    let enemy00 = new Enemy();
-  }
+    let enemy00 = new Enemy("Bear", 120, 0, 50, 100, 90, 100, 100);
+    let enemy01 = new Enemy("Dark Elver", 80, 90, 70, 40, 120, 100, 100);
+    let enemy02 = new Enemy("Dwarf", 140, 100, 120, 40, 75, 100, 100);
+    let enemy03 = new Enemy("Half Orc", 200, 70, 60, 40, 80, 100, 100);
+    let enemy04 = new Enemy("Nerubian", 180, 200, 65, 80, 120, 100, 100);
+    let enemy05 = new Enemy("Night Elver", 90, 150, 80, 60, 100, 100, 100);
+    let enemy06 = new Enemy("Orc", 200, 50, 80, 40, 110, 100, 100);
+    let enemy07 = new Enemy("Tauren", 350, 80, 40, 60, 80, 100, 100);
+    let enemy08 = new Enemy("Warewolf", 165, 110, 80, 150, 120, 100, 100);
+    let enemy08 = new Enemy("Warewolf", 165, 110, 80, 150, 120, 100, 100);
+    let enemy09 = new Enemy("Fire Elemental", 180, 110, 65, 95, 120, 100, 100);
 
+    let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(10));
+
+    switch (chooseRandomEnemy) {
+      case 0:
+        enemy = enemy00;
+        break;
+      case 1:
+        enemy = enemy01;
+        break;
+      case 2:
+        enemy = enemy02;
+        break;
+      case 3:
+        enemy = enemy03;
+        break;
+      case 4:
+        enemy = enemy04;
+        break;
+      case 5:
+        enemy = enemy05;
+        break;
+      case 6:
+        enemy = enemy06;
+        break;
+      case 7:
+        enemy = enemy07;
+        break;
+      case 8:
+        enemy = enemy08;
+        break;
+      case 9:
+        enemy = enemy09;
+        break;
+    }
+    getHeader.innerHTML = "<p>Task: Choose your move</p>";
+    getActions.innerHTML =
+      '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Attack</a>';
+    getEnemy.innerHTML =
+      '<img src="images/enemies/' +
+      enemy.enemyType.toLowerCase() +
+      '.png" alt="' +
+      enemy.enemyType +
+      '"class="img-avatar><div><h3>' +
+      enemy.enemyType +
+      '</h3><p class="health-enemy">Health: ' +
+      enemy.health +
+      "</p><p>Mana: " +
+      enemy.mana +
+      "</p><p>Strength: " +
+      enemy.strength +
+      "</p><p>Agility: " +
+      enemy.agility +
+      "</p><p>Speed: " +
+      enemy.speed +
+      "</p></div>";
+  },
 };
