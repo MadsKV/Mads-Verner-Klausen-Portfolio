11d17d46066162f7fdeb9b971dc1c086a67be411
Mads <mads.v.k@hotmail.com>
2020-11-16 08:54:58 +0100

    Css added

--- JavaScript Projects/Battle-Arena/js/player.js
+++ JavaScript Projects/Battle-Arena/js/player.js
@@ -67,5 +67,29 @@ let PlayerMoves = {
                 }
             }
         }
+        else if (getEnemySpeed >= getPlayerSpeed) {
+            let enemyAttackValues = enemyAttack();
+            let totalDamage = enemyAttackValues[0] * enemyAttackValues[1];
+            player.health = player.health - totalDamage;
+            alert("Enemy hit " + enemyAttackValues[0] + " damage " + enemyAttackValues[1] + " times.");
+            if (player.health <= 0) {
+                alert("You lost! Refresh browser to play again.");
+                getEnemyHealth.innerHTML = 'Health: ' + enemy.health;
+                getPlayerHealth.innerHTML = 'Health: 0';
+            } else {
+                getPlayerHealth.innerHTML = 'Health: ' + player.health;
+                let playerAttackValues = playerAttack();
+                let totalDamage = playerAttackValues[0] * playerAttackValues[1];
+                enemy.health = enemy.health - totalDamage;
+                alert("You hit " + playerAttackValues[0] + " damage " + playerAttackValues[1] + " times.");
+                if (enemy.health <= 0) {
+                    alert("You win! Refresh browser to play again.");
+                    getEnemyHealth.innerHTML = 'Health: 0';
+                    getPlayerHealth.innerHTML = 'Health: ' + player.health;
+                } else {
+                    getEnemyHealth.innerHTML = 'Health: ' + enemy.health;
+                }
+            }
+        }
     }
 }
