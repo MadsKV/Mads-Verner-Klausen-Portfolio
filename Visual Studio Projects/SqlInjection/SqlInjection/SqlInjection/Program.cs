ad8af6d828b184b4b90bed5f3e6950650087b629
Mads <mads.v.k@hotmail.com>
2021-01-19 13:32:32 +0100

    sqlinjection

---
+++ Visual Studio Projects/SqlInjection/SqlInjection/SqlInjection/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace SqlInjection
+{
+    class Program
+    {
+        public static SqlConnection Connection()
+        {
+            SqlConnectionStringBuilder builder =
+            new SqlConnectionStringBuilder();
+
+            builder.DataSource = @"MADS-V-KLAUSEN\MSSQLSERVER01";
+            builder.InitialCatalog = "School101";
+            builder.IntegratedSecurity = true;
+
+            SqlConnection connection = new SqlConnection(builder.ConnectionString);
+            connection.Open();
+            return connection;
+        }
+        //Den farlige injection der er mulig at lave ved input 2 (StudentLastname) dette kan potentielt være DROP TABLE, DELETE FROM osv osv.
+        //sqlinjection', 1,2); CREATE TABLE Sqlinjection (Id int);--
+
+        //for at undgå at denne type injection kan bruges, skal man forebygge parameter hvilket bliver vist i "SecureVersion"
+        public static string insertStudent(string StudentFirstName, string StudentLastName, int StudentAge, int ClassID, SqlConnection connection)
+        {
+            try
+            {
+                string sql = @"
+                    INSERT INTO Student (StudentFirstName, StudentLastName, StudentAge, ClassID)
+                    VALUES ('{0}', '{1}', {2}, {3})";
+                string formatted = string.Format(sql, StudentFirstName, StudentLastName, StudentAge, ClassID);
+                SqlCommand command = new SqlCommand(formatted, connection);
+
+                int numberOfRowAffected = command.ExecuteNonQuery();
+                return numberOfRowAffected + "Rows was affected.\n";
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("SQL exception caught in DeleteById " + e.ToString());
+                Console.ReadLine();
+                return "0 rows affected.\n";
+            }
+        }
+        public static void Main(string[] args)
+        {
+            SqlConnection connection = Connection();
+
+            const ConsoleKey keyInfo1 = ConsoleKey.D1;
+
+
+            while (true)
+            {
+                Console.Clear();
+                Console.ForegroundColor
+                = ConsoleColor.DarkGray;
+                Console.WriteLine("" +
+                    "Press 1 :For Student Management.\n");
+
+                ConsoleKey pressedKey = PressedKey();
+
+                switch (pressedKey)
+                {
+                    case keyInfo1:
+                        Console.ForegroundColor
+                        = ConsoleColor.Blue;
+
+                        switch (pressedKey)
+                        {
+                            // The Second menu
+                            case keyInfo1:
+                                Console.ForegroundColor
+                                = ConsoleColor.Blue;
+                                Console.Clear();
+                                Console.WriteLine("" +
+                                "Press 1 :To Create a new Student");
+
+                                pressedKey = PressedKey();
+
+                                switch (pressedKey)
+                                {
+                                    case keyInfo1:
+                                        Console.ForegroundColor
+                                        = ConsoleColor.Blue;
+                                        Console.WriteLine("Enter the credentials of the student in this format : \n\n" +
+                                            ">First name, Last name, Age, ClassID<");
+                                        string CInput = Console.ReadLine();
+                                        string CInput2 = Console.ReadLine();
+                                        int CInput3 = int.Parse(Console.ReadLine());
+                                        int CInput4 = int.Parse(Console.ReadLine());
+                                        insertStudent(CInput, CInput2, CInput3, CInput4, connection);
+                                        break;
+                                }
+                                break;
+                        }
+                        break;
+                }
+                break;
+                ConsoleKey PressedKey()
+                {
+                    do
+                    {
+                        while (!Console.KeyAvailable) ;
+                        ConsoleKey pressed = Console.ReadKey(true).Key;
+                        if (pressed == keyInfo1)
+                            return pressed;
+                        Console.Clear();
+                    } while (true);
+
+                }
+            }
+        }
+    }
+}
---
+++ Visual Studio Projects/SqlInjection/SqlInjection/SqlInjection/SecureVersion.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace SqlInjection
+{
+    class SecureVersion
+    {
+        public static SqlConnection openConnection()
+        {
+            SqlConnectionStringBuilder builder =
+            new SqlConnectionStringBuilder();
+
+            builder.DataSource = @"MADS-V-KLAUSEN\MSSQLSERVER01";
+            builder.InitialCatalog = "School101";
+            builder.IntegratedSecurity = true;
+
+            SqlConnection connection = new SqlConnection(builder.ConnectionString);
+            connection.Open();
+            return connection;
+        }
+        public static void CreateStudent(string StudentFirstName, string StudentLastName, int StudentAge, int ClassID)
+        {
+            SqlConnection conn = openConnection(); // Åbner forbindelse til databasen
+
+
+
+            // Initialiserer en ny instans af SqlClient build-in funktion fra System.Data.SqlClient;
+            using SqlCommand command = new SqlCommand(@"INSERT INTO Student
+                                                      (StudentFirstname, StudentLastName, StudentAge, ClassID)
+                                                      VALUES(@StudentFirstName, @StudentLastName, @StudentAge, @ClassID)", conn);
+            {
+                command.Parameters.Add("@StudentFirstName", SqlDbType.NVarChar, 32).Value = StudentFirstName;
+                command.Parameters.Add("@StudentLastName", SqlDbType.NVarChar, 32).Value = StudentLastName;
+                command.Parameters.Add("@studentAge", SqlDbType.Int).Value = StudentAge;
+                command.Parameters.Add("@ClassID", SqlDbType.Int).Value = ClassID;
+                command.ExecuteNonQuery();
+                conn.Close(); // Lukker forbindelsen til databasen når query er kørt.
+            }
+        }
+            public static void Main(string[] args)
+            {
+                const ConsoleKey keyInfo1 = ConsoleKey.D1;
+
+                while (true)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor
+                    = ConsoleColor.DarkGray;
+                    Console.WriteLine("" +
+                        "Press 1 :For Student Management.\n");
+
+                    ConsoleKey pressedKey = PressedKey();
+
+                    switch (pressedKey)
+                    {
+                        case keyInfo1:
+                            Console.ForegroundColor
+                            = ConsoleColor.Blue;
+
+                            switch (pressedKey)
+                            {
+                                // The Second menu
+                                case keyInfo1:
+                                    Console.ForegroundColor
+                                    = ConsoleColor.Blue;
+                                    Console.Clear();
+                                    Console.WriteLine("" +
+                                    "Press 1 :To Create a new Student");
+
+                                    pressedKey = PressedKey();
+
+                                    switch (pressedKey)
+                                    {
+                                        case keyInfo1:
+                                            Console.ForegroundColor
+                                            = ConsoleColor.Blue;
+                                            Console.WriteLine("Enter the credentials of the student in this format : \n\n" +
+                                                ">First name, Last name, Age, ClassID<");
+                                            string CInput = Console.ReadLine();
+                                            string CInput2 = Console.ReadLine();
+                                            int CInput3 = int.Parse(Console.ReadLine());
+                                            int CInput4 = int.Parse(Console.ReadLine());
+                                            CreateStudent(CInput, CInput2, CInput3, CInput4);
+                                            break;
+                                    }
+                                    break;
+                            }
+                            break;
+                    }
+                    break;
+                    ConsoleKey PressedKey()
+                    {
+                        do
+                        {
+                            while (!Console.KeyAvailable) ;
+                            ConsoleKey pressed = Console.ReadKey(true).Key;
+                            if (pressed == keyInfo1)
+                                return pressed;
+                            Console.Clear();
+                        } while (true);
+
+                    }
+                }
+            }
+    }
+}
+    
+
