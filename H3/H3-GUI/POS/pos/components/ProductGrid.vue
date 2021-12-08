41f2d915c632d39dd8d7241aa3010f58bb7874fa
MadsKV <mads.v.k@hotmail.com>
2021-12-08 10:58:34 +0100

    Json

---
+++ H3/H3-GUI/POS/pos/components/ProductGrid.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper">
+    <div>One</div>
+    <div>Two</div>
+    <div>Three</div>
+    <div>Four</div>
+    <div>Five</div>
+    <div>Six</div>
+    <div>Seven</div>
+    <div>Eight</div>
+    <div>Nine</div>
+    <div>Ten</div>
+    <div>Elleven</div>
+    <div>Twelve</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.wrapper {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-auto-rows: 200px;
+}
+</style>
---
+++ H3/H3-GUI/POS/pos/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return { Products: [] };
+  },
+  mounted() {
+    const axios = require("axios").default;
+
+    axios.get("/Products.json").then((Products) => (this.Products = Products));
+  },
+};
+</script>
