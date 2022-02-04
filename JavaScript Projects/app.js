758fea75902018f9a8cf8c5679e1f7986f14f7c4
MadsKV <mads.v.k@hotmail.com>
2022-02-04 08:33:04 +0100

    css prac

--- JavaScript Projects/app.js
+++
@@ -1,7 +0,0 @@
-const banner = document.getElementsByClassName('banner')[0];
-const blocks = document.getElementsByClassName('blocks');
-
-for(var i = 1; i < 400; i++){
-    banner.innerHTML += `<div class='blocks'></div>`;
-    blocks[i].style.animationdelay = `${i * 0.05}s`;
-}
