f9b544d602d2549d06affb055846832a0c252fd1
MadsKV <mads.v.k@hotmail.com>
2022-02-16 13:56:03 +0100

    api

---
+++ JavaScript Projects/Portfolio/Login System API Project/API/LoginAPI/LoginAPI/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LoginAPI.Data
+{
+    public class ApplicationUser: IdentityUser
+    {
+        [PersonalData]
+        public DateTime CareerStarted { get; set; }
+    }
+}
